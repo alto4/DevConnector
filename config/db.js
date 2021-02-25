2b8d8b12bbd8b5f502263df09e1dbbc468b3a107
Scott Alton <scottaltonmusic@gmail.com>
2021-02-25 14:43:19 -0500

    Connection established between express server and database.

---
+++ config/db.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("mongoURI");
+
+// Attempt to connect to database using connection string/mongoURI from config folder
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log("Successfully connected to MongoDB database...");
+  } catch (err) {
+    console.log(err.message);
+
+    // Exit connection process with failure code
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
