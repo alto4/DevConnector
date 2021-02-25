3d6efba56666e563dd0ff0e0ef106ca383e442dd
Scott Alton <scottaltonmusic@gmail.com>
2021-02-25 15:26:00 -0500

    Added users, auth, posts, profile routes to api. Basic setup of User model.

--- server.js
+++ server.js
@@ -7,8 +7,12 @@ const app = express();
 connectDB();
 
 // ROUTES
-
 app.get("/", (req, res) => res.send("API running successfuly."));
+
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
 
 // Listen on port specified in env file, otherwise list on port 5000
 const PORT = process.env.PORT || 5000;
